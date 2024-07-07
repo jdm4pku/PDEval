b269c71a4468182055f840d6f90543964b956a9c
jdm4pku <jdmpku@gmail.com>
2024-07-07 16:55:58 +0800

    remove cmd output

--- src/logger.py
+++ src/logger.py
@@ -8,10 +8,10 @@ def get_logger(name):
     file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s', '%m/%d/%Y %H:%M:%S')
     file_handler.setFormatter(file_formatter)
     # 创建一个用于终端显示的处理器
-    stream_handler = logging.StreamHandler()
-    stream_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s', '%m/%d/%Y %H:%M:%S')
-    stream_handler.setFormatter(stream_formatter)
+    # stream_handler = logging.StreamHandler()
+    # stream_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s', '%m/%d/%Y %H:%M:%S')
+    # stream_handler.setFormatter(stream_formatter)
     # 将两个处理器添加到日志记录器
     logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
+    # logger.addHandler(stream_handler)
     return logger
